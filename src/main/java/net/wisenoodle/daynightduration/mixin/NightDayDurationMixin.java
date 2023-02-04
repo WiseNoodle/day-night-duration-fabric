f95559403c484a517da1ad221604c2f3f5aa197c
WiseNoodle <95942443+WiseNoodle@users.noreply.github.com>
2023-02-03 22:55:35 -0500

    Initial commit

---
+++ src/main/java/net/wisenoodle/daynightduration/mixin/NightDayDurationMixin.java
@@ -0,0 +1,16 @@
+package net.wisenoodle.daynightduration.mixin;
+
+import net.wisenoodle.daynightduration.DayNightDuration;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		DayNightDuration.LOGGER.info("Mod loaded successfully!");
+	}
+}
